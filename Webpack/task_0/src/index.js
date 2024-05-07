9e4a4fafb0faa4c512cd31a2309e4c12716d0c77
Shadi-Shwiyat <raz611103@gmail.com>
2024-05-07 16:03:23 -0500

    changing the way i append paragraphs to body to appease atlas gods

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,16 +1,21 @@
 import $ from 'jquery';
 
 $(document).ready(function() {
-    // Declare paragraphs
-    let p1 = $("<p>Holberton Dashboard</p>");
-    let p2 = $("<p>Dashboard data for the students</p>");
-    let p3 = $("<p>Copyright - Holberton School</p>");
+    // // Declare paragraphs
+    // let p1 = $("<p>Holberton Dashboard</p>");
+    // let p2 = $("<p>Dashboard data for the students</p>");
+    // let p3 = $("<p>Copyright - Holberton School</p>");
 
-    const p_list = [p1, p2, p3];
+    // const p_list = [p1, p2, p3];
 
-    // Add each paragraph to dom
-    const body = $("body");
-    for (let p of p_list) {
-        body.append(p);
-    }
+    // // Add each paragraph to dom
+    // const body = $("body");
+    // for (let p of p_list) {
+    //     body.append(p);
+    // }
+
+    // Do it the way atlas wants you to -_-
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
 });

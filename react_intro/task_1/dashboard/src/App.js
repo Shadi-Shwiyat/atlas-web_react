2247194d09abdc2549e4d56ab8e4cebebcc90689
Shadi-Shwiyat <raz611103@gmail.com>
2024-05-07 23:58:19 -0500

    task 2 creating a form with email and password

--- react_intro/task_1/dashboard/src/App.js
+++ react_intro/task_1/dashboard/src/App.js
@@ -4,6 +4,13 @@ import './styles/App.css';
 import logo from './assets/logo.jpg';
 
 function App() {
+  const handleLabelClick = (inputId) => {
+    const inputElement = document.getElementById(inputId);
+    if (inputElement) {
+      inputElement.focus();
+    }
+  };
+
   return (
     <div className="App">
       <div className="App-header">
@@ -12,6 +19,15 @@ function App() {
       </div>
       <div className='App-body'>
         <p>Login to access the full dashboard</p>
+        <div className='sign-in'>
+          <label htmlFor="email" onClick={() => handleLabelClick("email")}>Email:</label>
+          <input type="email" id="email" name="email" />
+          <br />
+          <label htmlFor="password" onClick={() => handleLabelClick("password")}>Password:</label>
+          <input type="password" id="password" name="password" />
+          <br />
+          <button>OK</button>
+        </div>
       </div>
       <div className='App-footer'>
         <p>{getFooterCopy(true)} - {getFullYear()}</p>

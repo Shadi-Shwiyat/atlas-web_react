f80209ea40153b12157254934d2c3cf8167be844
Shadi-Shwiyat <raz611103@gmail.com>
2024-05-07 19:18:14 -0500

    trying to get holberton logo to show up

---
+++ Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,31 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import '../css/main.css';
+
+$(document).ready(function() {
+    // DOM manipulation
+    $('body').append('<div id="logo"></div>');
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button id="startButton">Click here to get started</button>');
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+
+    // Counter variable
+    let count = 0;
+
+    // Function to update the counter
+    function updateCounter() {
+        count = count + 1;
+        const count_p = $('#count');
+        count_p.text(count);
+    }
+
+    // Debounced function to update the counter with a 500ms delay
+    const debouncedUpdateCounter = _.debounce(updateCounter, 500);
+
+    // Event handling for the button with debounce
+    $('#startButton').click(function() {
+        debouncedUpdateCounter();
+    });
+})
---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production', // Set Webpack mode to production
+  entry: './js/dashboard_main.js', // Entry point of your application
+  output: {
+    path: path.resolve(__dirname, 'public'), // Output directory
+    filename: 'bundle.js' // Output filename
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Rule for .css files
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/, // Rule for image files
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      }
+    ]
+  }
+};

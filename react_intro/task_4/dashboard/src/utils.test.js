92ce60bd04785bfa641ca2d5b321ea2202bb0426
Shadi-Shwiyat <raz611103@gmail.com>
2024-05-08 14:31:51 -0500

    created build for task 4, pushing to git hub pages

---
+++ react_intro/task_4/dashboard/src/utils.test.js
@@ -0,0 +1,26 @@
+import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
+
+describe('getFullYear', () => {
+  test('returns the correct year', () => {
+    const currentYear = new Date().getFullYear();
+    expect(getFullYear()).toEqual(currentYear);
+  });
+});
+
+describe('getFooterCopy', () => {
+  test('returns "Holberton School" when the argument is true', () => {
+    expect(getFooterCopy(true)).toEqual('Holberton School');
+  });
+
+  test('returns "Holberton School main dashboard" when the argument is false', () => {
+    expect(getFooterCopy(false)).toEqual('Holberton School main dashboard');
+  });
+});
+
+describe('getLatestNotification', () => {
+  test('returns the correct string', () => {
+    // Assuming getLatestNotification always returns a specific string
+    const expectedNotification = '<strong>Urgent requirement</strong> - complete by EOD';
+    expect(getLatestNotification()).toEqual(expectedNotification);
+  });
+});

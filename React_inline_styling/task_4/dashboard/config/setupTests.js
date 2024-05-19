ee410f33599c26dd96fdd1f36fe5d8cf8a017e43
Shadi-Shwiyat <raz611103@gmail.com>
2024-05-18 21:38:18 -0500

    task 4 added a animation to the notification menu, added onclick function to open and close notification menu

---
+++ React_inline_styling/task_4/dashboard/config/setupTests.js
@@ -0,0 +1,21 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { JSDOM } from 'jsdom';
+
+// Set up a DOM environment
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+
+// Configure Enzyme
+configure({ adapter: new Adapter() });
---
+++ React_inline_styling/task_4/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,151 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import NotificationItem from './NotificationItem.js';
+import { StyleSheet, css } from 'aphrodite';
+import NotificationItemShape from './NotificationItemShape';
+
+const opacityKeyframes = {
+  '0%': { opacity: 0.5 },
+  '100%': { opacity: 1 }
+};
+
+const bounceKeyframes = {
+  '0%': { transform: 'translateY(0)' },
+  '50%': { transform: 'translateY(-5px)' },
+  '100%': { transform: 'translateY(5px)' }
+};
+
+const styles = StyleSheet.create({
+  menuItem: {
+    position: 'fixed',
+    top: 0,
+    right: 0,
+    marginTop: '10px',
+    marginRight: '20px',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+    zIndex: 30,
+    ':hover': {
+      animationName: [opacityKeyframes, bounceKeyframes],
+      animationDuration: '1s, 0.5s',
+      animationIterationCount: '3, 3',
+      animationTimingFunction: 'ease-in-out, ease-in-out'
+    }
+  },
+  notifications: {
+    position: 'fixed',
+    border: '1px dashed #E0354B',
+    backgroundColor: '#fff8f8',
+    padding: '10px',
+    top: '60px',
+    right: '20px',
+    zIndex: '33',
+    '@media (max-width: 900px)': {
+      top: '20px',
+      left: '0px',
+      width: '100vw',
+      height: '100vh',
+      border: 'none',
+      padding: '0px',
+      backgroundColor: 'white',
+    }
+  },
+  notificationsUl: {
+    '@media (max-width: 900px)': {
+      padding: 0
+    }
+  },
+  notificationsP: {
+    '@media (max-width: 900px)': {
+      fontSize: '20px',
+      margin: '0',
+      padding: '0'
+    }
+  },
+  notificationButton: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    padding: '5px',
+    paddingRight: '10px',
+    border: 'none',
+    background: 'none',
+    cursor: 'pointer',
+    appearance: 'none',
+    outline: 'none',
+    zIndex: '39',
+    '@media (max-width: 900px)': {
+      top: '-19px',
+      right: '6px'
+    }
+  }
+});
+
+class Notifications extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayDrawer: props.displayDrawer
+    };
+    this.toggleDrawer = this.toggleDrawer.bind(this);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // Check if the length of the new listNotifications is greater than the previous one
+    return (
+      nextProps.listNotifications.length > this.props.listNotifications.length ||
+      nextState.displayDrawer !== this.state.displayDrawer
+    );
+  }
+
+  toggleDrawer() {
+    this.setState(prevState => ({
+      displayDrawer: !prevState.displayDrawer
+    }));
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  render() {
+    const { listNotifications = [] } = this.props;
+    const { displayDrawer } = this.state;
+
+    return (
+      <>
+        <div className={css(styles.menuItem)} id='menuItem' onClick={this.toggleDrawer}>
+          <p>Your notifications</p>
+        </div>
+        {displayDrawer && (
+          <div className={css(styles.notifications)} id='notifications'>
+            <button
+              className={css(styles.notificationButton)}
+              aria-label="Close"
+              onClick={this.toggleDrawer}
+            >
+              x
+            </button>
+            <p className={css(styles.notificationsP)}>Here is the list of notifications</p>
+            <ul className={css(styles.notificationsUl)}>
+              {listNotifications.length === 0 ? (
+                <NotificationItem type="default" value="No new notification for now" />
+              ) : (
+                listNotifications.map(notification => (
+                  <NotificationItem key={notification.id} {...notification} markAsRead={this.markAsRead} />
+                ))
+              )}
+            </ul>
+          </div>
+        )}
+      </>
+    );
+  }
+}
+
+Notifications.propTypes = {
+  listNotifications: PropTypes.arrayOf(NotificationItemShape)
+};
+
+export default Notifications;

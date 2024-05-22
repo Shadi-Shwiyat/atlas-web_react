502141940dbd024e6b6c73bb55f18b85d2fff351
Shadi-Shwiyat <raz611103@gmail.com>
2024-05-22 16:18:53 -0500

    task 0 reading data from a json file and function that filters data based on matching author and user id

---
+++ 0x08_react_redux_action_creator_normalizr/task_0/dashboard/src/Login/Login.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Login from './Login';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+describe('Login Component', () => {
+  let user;
+  
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+    user = {
+      email: '',
+      password: '',
+      isLoggedIn: false,
+    }
+  });
+
+  it('renders without crashing', () => {
+    shallow(<Login user={user}/>);
+  });
+
+  it('renders 2 input and 2 label tags', () => {
+    const wrapper = shallow(<Login user={user}/>);
+    expect(wrapper.find('input[type="email"]').length).toEqual(1);
+    expect(wrapper.find('input[type="password"]').length).toEqual(1);
+    expect(wrapper.find('label').length).toEqual(2);
+  });
+
+  it('submit button is disabled by default', () => {
+    const wrapper = shallow(<Login user={user}/>);
+    const submitButton = wrapper.find('input[type="submit"]');
+    expect(submitButton.prop('disabled')).toBe(true);
+  });
+
+  it('submit button is enabled after changing the value of the two inputs', () => {
+    const wrapper = mount(<Login user={user}/>);
+    const emailInput = wrapper.find('input[type="email"]');
+    const passwordInput = wrapper.find('input[type="password"]');
+    const submitButton = wrapper.find('input[type="submit"]');
+
+    // Simulate user input for email and password
+    emailInput.simulate('change', { target: { value: 'test@example.com' } });
+    passwordInput.simulate('change', { target: { value: 'password123' } });
+
+    // Check if the submit button is enabled
+    expect(wrapper.find('input[type="submit"]').prop('disabled')).toBe(false);
+  });
+});

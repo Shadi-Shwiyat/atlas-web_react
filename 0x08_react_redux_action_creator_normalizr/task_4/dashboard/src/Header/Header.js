8976f20aa70fecc057027c04e63679ff5efdb4f6
Shadi-Shwiyat <raz611103@gmail.com>
2024-05-22 19:00:13 -0500

    task 4 creating actions for UI and testing them

---
+++ 0x08_react_redux_action_creator_normalizr/task_4/dashboard/src/Header/Header.js
@@ -0,0 +1,70 @@
+import React, { useContext } from 'react';
+import logo from '../assets/logo.jpg';
+import {StyleSheet, css} from 'aphrodite';
+import AppContext from '../App/AppContext';
+
+const styles = StyleSheet.create({
+  header: {
+    margin: 0,
+  },
+  appHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    position: 'relative',
+    margin: 0,
+    '::after': {
+      content: "''",
+      position: 'fixed',
+      top: '239px',
+      left: 'calc(10px)',
+      width: 'calc(100% - 20px)',
+      height: '3px',
+      backgroundColor: '#E0354B',
+    },
+  },
+  h1: {
+    fontFamily: 'arial',
+    fontWeight: 'bolder',
+    fontSize: '36px',
+    color: '#E0354B',
+    marginLeft: '30px',
+  },
+  logo: {
+    width: '230px',
+    height: '230px',
+  },
+  welcomeMessage: {
+    position: 'absolute',
+    right: '10px',
+    top: -6,
+    fontSize: '18px',
+    zIndex: 39
+  },
+  logoutLink: {
+    color: '#E0354B',
+    cursor: 'pointer',
+  },
+});
+
+
+function Header() {
+  const { user, logOut } = useContext(AppContext);
+
+  const handleLogout = () => {
+    logOut();
+  };
+
+  return (
+    <div className={css(styles.appHeader)}>
+      <img src={logo} alt='logo.jpg' className={css(styles.logo)}></img>
+      <h1 className={css(styles.h1)}>School dashboard</h1>
+      {user.isLoggedIn && (
+        <div className={css(styles.welcomeMessage)} id="logoutSection">
+          Welcome {user.email} (<span className={css(styles.logoutLink)} onClick={handleLogout} id='logoutLink'>logout</span>)
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Header;
